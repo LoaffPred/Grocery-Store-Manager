3e8ba6930f1ae0f282c47dbffc4701cf5ef59bb9
LoaffPred <icosidiouxxix@gmail.com>
2023-03-12 03:01:06 +0800

    commit

--- states/market.py
+++ states/market.py
@@ -1,11 +1,13 @@
 from states.state import State
 from util import *
+import random
 
 
 class Market(State):
     def __init__(self, game):
         super().__init__(game)
         self.stockpile = read_json("baseStockpile.json")
+        self.inflation()
 
     def update(self):
         self.render()
@@ -14,6 +16,7 @@ class Market(State):
         if a == "1":
             self.sell()
         elif a == "0":
+            self.game.player.days_played += 1
             self.exit_state()
         else:
             print("Invalid input...")
@@ -28,8 +31,8 @@ class Market(State):
         market_table = self.game.to_table(self.stockpile)
         playertable = self.game.to_table(self.game.player.stockpile)
 
-        basetable[0][0] = playertable
-        basetable[0][1] = market_table
+        basetable.rows[0][0] = playertable
+        basetable.rows[0][1] = market_table
 
         return basetable
 
@@ -59,4 +62,16 @@ class Market(State):
         # randomly choose an item to inflate
         # randomly choose whether to inflate or deflate
         # randomly choose by how much 1%, 2%, or 3%
-        pass
+        items = random.sample(list(self.stockpile.keys()), k=random.randint(3, 5))
+        for item in items:
+            option = random.choice(["inflate", "deflate"])
+            chance = random.uniform(1, 3)
+            percentage = (chance / 100) + 1
+            if option == "inflate":
+                self.stockpile[item]["price"] = round(
+                    self.stockpile[item]["price"] * percentage
+                )
+            else:
+                self.stockpile[item]["price"] = round(
+                    self.stockpile[item]["price"] / percentage
+                )
